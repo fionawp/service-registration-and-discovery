35b1acee766658b13ab16a1ca0f6f29e43773c35
fionawp <fionawp@126.com>
2019-09-09 09:49:03 +0800

    perfection heartbeat

--- server/server.go
+++ server/server.go
@@ -28,41 +28,36 @@ func Start(conf *context.Config) {
 	registerRoutes(app, conf)
 
 	ip := GetIp()
-	//注册服务
-	_, serviceErr := service.RegisterServer(conf, service.ServerInfo{
+	thisServer := service.ServerInfo{
 		ServiceName: conf.ServiceName(),
 		Ip:          ip,
 		Port:        strconv.Itoa(conf.HttpServerPort()),
-		Desc:        "这是一个测试server",
+		Desc:        "this is a test server",
 		UpdateTime:  time.Now(),
 		CreateTime:  time.Now(),
 		Ttl:         5,
-	})
-
+	}
+	//注册服务
+	_, serviceErr := service.RegisterServer(conf, thisServer)
 	if serviceErr != nil {
-		conf.GetLog().Info("注册服务异常 {}", serviceErr.Error())
-		panic("注册服务异常")
+		conf.GetLog().Error("register server exception {}", serviceErr.Error())
+		panic("register server exception")
 	}
 
-	thisServer := findServer(conf, ip+":"+strconv.Itoa(conf.HttpServerPort()), conf.ServiceName())
-
-	if thisServer != nil {
-		ttl := thisServer.Ttl
-		ticker := time.NewTicker(time.Duration(ttl) * time.Second)
-		go func() {
-			for {
-				select {
-				case <-ticker.C:
-					modServer(conf, thisServer)
-				}
-			}
-		}()
-	}
+	//every ttl once heartbeat
+	ttl := thisServer.Ttl
+	heartBeat(ttl, func() {
+		thisServer.UpdateTime = time.Now()
+		_, modServerErr := service.RegisterServer(conf, thisServer)
+		if modServerErr != nil {
+			conf.GetLog().Error("heart beat err: " + modServerErr.Error())
+		}
+	})
 
 	app.Run(fmt.Sprintf("%s:%d", conf.HttpServerHost(), conf.HttpServerPort()))
 }
 
-//todo 获取本机的实际ip
+//todo get server ip;  gin port auto assign
 func GetIp() string {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
@@ -72,7 +67,7 @@ func GetIp() string {
 
 	ips := make([]string, 0)
 	for _, address := range addrs {
-		// 检查ip地址判断是否回环地址
+		// if ip isLoopback
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
 				ips = append(ips, ipnet.IP.String())
@@ -82,47 +77,15 @@ func GetIp() string {
 	return ips[0]
 }
 
-func findServer(conf *context.Config, serverName string, serviceName string) *service.ServerInfo {
-	serverInfo, err := service.FindServerByServerNameServiceName(conf, serverName, serviceName)
-
-	if err != nil {
-		conf.GetLog().Error("heart beat err: " + err.Error())
-		return nil
-	}
-
-	if serverInfo == nil {
-		conf.GetLog().Error("heart beat error: can not find the server: serviceName " +
-			serviceName + " servername " + serverName)
-		return nil
-	}
-
-	return serverInfo
-}
-
-func modServer(conf *context.Config, serverInfo *service.ServerInfo) {
-	_, serviceErr := service.RegisterServer(conf, service.ServerInfo{
-		ServiceName: serverInfo.ServiceName,
-		Ip:          serverInfo.Ip,
-		Port:        serverInfo.Port,
-		Desc:        serverInfo.Desc,
-		UpdateTime:  time.Now(),
-		CreateTime:  serverInfo.CreateTime,
-		Ttl:         serverInfo.Ttl,
-	})
-
-	if serviceErr != nil {
-		conf.GetLog().Error("heart beat error " + serviceErr.Error())
-	}
-}
-
-/*func heartBeat(interval int, callback func(conf *context.Config, serverInfo *service.ServerInfo)) {
-	ticker := time.NewTicker(time.Duration(interval)*time.Second)
+//heartbeat ticker
+func heartBeat(interval int, callback func()) {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
 	go func() {
 		for {
 			select {
-				case <- ticker.C :
-					callback(conf, serverInfo)
+			case <-ticker.C:
+				callback()
 			}
 		}
 	}()
-}*/
+}
